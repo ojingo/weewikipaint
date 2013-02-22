6ed109d2bba9ab4980aa51730091fb881cf8675a
TJ <tj@ojingolabs.com>
2013-02-22 11:39:39 +0100

    added a dependency and had it run before the task

--- jakefile.js
+++ jakefile.js
@@ -1,5 +1,10 @@
 desc("Example Description!");
 
-task("example", function() {
+task("example",["Dependency!"], function() {
     console.log("An Example Task!");
 });
+
+task("Dependency!", function() {
+    console.log("I am a Dependency!");
+});
+
