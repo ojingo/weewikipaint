f61381aeb2814a2ed9d68d0bb838aa4d798f0331
TJ <tj@ojingolabs.com>
2013-02-22 19:20:42 +0100

    removed _lint_runner_test and  modified for basic working

--- jakefile.js
+++ jakefile.js
@@ -1,3 +1,6 @@
+/*global desc, task, jake, fail, complete */
+"use strict";
+
 task("default", ["lint"]);
 
 desc("Lint Everything Description!");
@@ -5,5 +8,13 @@ desc("Lint Everything Description!");
 task("lint",[], function() {
     console.log("Linting code goes here!");
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js", {}, {});
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+
+    var options = {
+      node: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
 });
