aa2d6ab0452da572f80993e29af4ec17b86f6157
TJ <tj@ojingolabs.com>
2013-02-22 19:00:37 +0100

    Initial support for Lint

--- jakefile.js
+++ jakefile.js
@@ -4,4 +4,6 @@ desc("Lint Everything Description!");
 
 task("lint",[], function() {
     console.log("Linting code goes here!");
+    var lint = require("./build/lint/lint_runner.js");
+    lint.validateFile("jakefile.js", {}, {});
 });
