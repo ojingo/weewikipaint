700bd7b723a8900caa8b3cb6dcab6035ec558ea4
TJ <tj@ojingolabs.com>
2013-02-22 20:17:04 +0100

    tinkering with jakefile.js to match the sloppy tutorial explanations

--- jakefile.js
+++ jakefile.js
@@ -1,20 +1,37 @@
 /*global desc, task, jake, fail, complete */
-"use strict";
+(function() {
+    "use strict";
 
-task("default", ["lint"]);
+    task("default", ["lint"]);
 
-desc("Lint Everything Description!");
+    desc("Lint everything");
+    task("lint", [], function() {
+        var lint = require("./build/lint/lint_runner.js");
 
-task("lint",[], function() {
-    console.log("Linting code goes here!");
-    var lint = require("./build/lint/lint_runner.js");
-    var files = new jake.FileList();
-    files.include("**/*.js");
-    files.exclude("node_modules");
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+        lint.validateFileList(files.toArray(), options, {});
+    });
 
-    var options = {
-      node: true
-    };
-
-    lint.validateFileList(files.toArray(), options, {});
-});
+    function nodeLintOptions() {
+        return {
+            bitwise:true,
+            curly:false,
+            eqeqeq:true,
+            forin:true,
+            immed:true,
+            latedef:true,
+            newcap:true,
+            noarg:true,
+            noempty:true,
+            nonew:true,
+            regexp:true,
+            undef:true,
+            strict:true,
+            trailing:true,
+            node:true
+        };
+    }
+}());
